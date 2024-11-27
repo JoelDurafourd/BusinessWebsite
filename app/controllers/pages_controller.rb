be929c61d05cb39fb8239af065b85ff29156104d
JoelDurafourd <joel.durafourd@gmail.com>
2024-11-27 20:22:44 +0900

    added some basic pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def language_learning
+  end
+
+  def programming_services
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "pages#home"
+
+  get "pages/language_learning"
+
+  get "pages/programming_services"
 end
