159138ed40457033806e73b06661da4244827b29
JoelDurafourd <joel.durafourd@gmail.com>
2024-12-03 16:46:32 +0900

    before changing the application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+    # Can be used by load balancers and uptime monitors to verify that the app is live.
+    get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  root to: "pages#home"
+    # Defines the root path route ("/")
+    # root "posts#index"
 
-  get "pages/language_learning"
+    root to: "pages#home"
 
-  get "pages/programming_services"
+    get "pages/language_learning"
+
+    get "pages/programming_services"
+  end
 end
